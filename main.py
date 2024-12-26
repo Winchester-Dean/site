0bdf6e3cba609f76013cb14e421285813c3ad209
root <root@yakuboff.deanchik.ru>
2024-12-27 02:09:34 +0800

    bugs fixed

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import asyncio
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-async def home():
-    return render_template('index.html') 200
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         port=443,
         ssl_context=(
-            'path/to/server.crt',
-            'path/to/server.key'
+            '/etc/letsencrypt/live/deanchik.ru/fullchain.pem',
+            '/etc/letsencrypt/live/deanchik.ru/privkey.pem'
         ),
         debug=True,
         use_reloader=True
